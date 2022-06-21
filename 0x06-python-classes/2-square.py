67293a796aee4f84ccf620343aa05776b2c0e1d0
TheSeekerSeeker <odondiezra5@gmail.com>
2022-06-21 22:24:19 +0300

    0x06. Python - Classes and Objects

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
+"""
+Module 2-square
+Defines class Square with private attribute size and validates size
+"""
 
 
 class Square:
-    '''Class Square object intialized with size
-    '''
-    pass
+    """
+    class Square definition
+    Args:
+        size (int): size of a side in square
+    """
 
     def __init__(self, size=0):
-        '''init method of class Square
-        '''
-        if type(size) != int:
+        """
+        Initializes square
+        Attributes:
+            __size (int): size of a side of square, defaults to 0 if none
+        """
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
