b5684727973ebe47bb76c2eb16b447eb6cfd7edd
TheSeekerSeeker <odondiezra5@gmail.com>
2022-06-21 22:07:37 +0300

    0x06. Python - Classes and Objects

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
+"""
+Module 1-square
+Defines class Square with private attribute size
+"""
 
 
 class Square:
     """
-    class Square with private instance attribute size
+    class Square definition
+    Args:
+        size : size of a side in square
     """
     def __init__(self, size):
-        self.__size = size
+        """
+        Initializes square
+        Attributes:
+            size: size of a side of square
+        """
+        self.__size = size:
